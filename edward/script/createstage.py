ee64984c8f99226139596203f543debd3ae840dc
Xiaowei (Edward) Zhang <edwardzxw@qq.com>
2021-05-28 11:12:06 +0800

    create image/label dir if not exist

--- edward/script/createstage.py
+++ edward/script/createstage.py
@@ -12,7 +12,9 @@ def copy_labled_image(file_name, trainset_dir, images_dir, labels_dir):
     image_path = os.path.join(trainset_dir, image_name)
     label_path = os.path.join(trainset_dir, file_name)
     shutil.copyfile(image_path, os.path.join(images_dir, image_name))
+    print("Copied " + image_path)
     shutil.copyfile(label_path, os.path.join(labels_dir, file_name))
+    print("Copied " + label_path)
     return image_name
 
 stage_dir = '/mnt/c/cygwin64/home/Edward/.virtualenvs/github/darknet'
@@ -21,19 +23,24 @@ labels_dir = os.path.join(stage_dir, 'data/baa/labels/')
 train_txt_path = os.path.join(stage_dir, 'data/baa/train.txt')
 val_txt_path = os.path.join(stage_dir, 'data/baa/val.txt')
 
-#archive_base_dir = '/mnt/c/cygwin64/home/Edward/.virtualenvs/hualiang/bone_age_algorithm/Sdk/archive'
-archive_base_dir = '/mnt/hualiang_dev_shared/darknet/archive'
-
 # get a list of all yolo label txt files
-trainset_dir = os.path.join(archive_base_dir, 'boneage-training-dataset/boneage-training-dataset')
+#archive_base_dir = '/mnt/c/cygwin64/home/Edward/.virtualenvs/hualiang/bone_age_algorithm/Sdk/archive'
+#archive_base_dir = '/mnt/hualiang_dev_shared/darknet/archive'
+#trainset_dir = os.path.join(archive_base_dir, 'boneage-training-dataset/boneage-training-dataset')
+
+trainset_dir = '/mnt/c/cygwin64/home/Edward/.virtualenvs/github/rsna_images'
 
 images_relpath_list = []
+
+os.mkdir(images_dir)
+os.mkdir(labels_dir)
 
 for filename in os.listdir(trainset_dir):
     f = os.path.join(trainset_dir, filename)
     if os.path.isfile(f) and filename.endswith('.txt'):
         if filename == 'classes.txt':
             shutil.copyfile(f, os.path.join(labels_dir, filename))
+            print("Copied classes.txt")
         else:
             image_name = copy_labled_image(filename, trainset_dir, images_dir, labels_dir)
             images_relpath_list.append('data/baa/images/' + image_name)
@@ -43,7 +50,9 @@ print(images_relpath_list)
 with open(train_txt_path, 'w') as f:
     for i in range(len(images_relpath_list) - 1):
         f.write("%s\n" % images_relpath_list[i])
+print("Updated " + train_txt_path)
 
 with open(val_txt_path, 'w') as f:
     for i in range(len(images_relpath_list) - 1, len(images_relpath_list)):
         f.write("%s\n" % images_relpath_list[i])
+print("Updated " + val_txt_path)
